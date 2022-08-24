69a5078f861ea6872fa9bc84b072077da43c1063
Aditya <aditya.pandey.241103@gmail.com>
2022-08-24 21:32:29 +0530

    Added operators.cpp

---
+++ C++ Source Codes/Operators.cpp
@@ -0,0 +1,67 @@
+/* operators in c++
+
+1. Arithmetic operators
+
+    A. Binary operators
+        (+) Addition
+        (-) Subtraction
+        (*) Multiplication             //These operators perform mathematical operations
+        (/) Division
+        (%) Modulo 
+        
+    B. Unary operators
+    
+    (++) Incrementer                  //Both of these are of two types pre and post
+    (--) Decrementer
+    
+    Syntax for pre Incrementer/Decrementer     ++<var> / --<var>
+    Syntax for post Incrementer/Decrementer    <var>++ / <var>--
+    
+
+2. Relational operators
+
+    (==) Gives true value if both operands have equal value 
+    (!=) Gives true value if both operands are unqual
+    
+    (>)
+    (<)             //Basic Mathematics comparison operators 
+    (<=)
+    (>=)
+    
+    
+3. Logical Operators 
+
+    (&&) And gives us true if both the operands are true, else false.
+    (||) OR gives us true if atleast one of the operands is true.
+    (!) NOT gives the opposite logical value of the operand.
+    
+    
+4. Assignment operator 
+
+
+    (=)  Assign value of right operand to the left operand 
+    (+=) Assign sum of two operands to the left operand 
+    (-=) Assign difference of two operands to the left operand
+    (*=) Assign multiplication of two operands to the left operand
+    (/=) Assign quotient of two operands to the left operand
+    
+    
+5. Miscellaneous Operators
+
+    {sizeof()}      Returns the size of a variable
+    {condition?X:Y} Returns value of X if condition is true or else value of Y 
+    {cast}          Converts one data type to another
+    (,)             Causes a sequence of operations to be performed
+    (&)             Returns the address of a variable
+    (*)             Pointer to a variable
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
